8d1193763f1248db4f01b8754628681485033fc2
Darshan Santosh Borse <90395345+DarshanBorse@users.noreply.github.com>
2022-04-24 17:53:43 +0530

    Updated

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,10 +1,10 @@
 import jsonwebtoken from "jsonwebtoken";
 
-export const authMiddleware = (req, res) => {
-    if (!headers['authorization']) {
+export const authMiddleware = (req, res, next) => {
+    if (!req.headers['authorization']) {
         return res.json({ message: "Please, Enter a token" });
     }
-    const token = Headers['authorization'].split(' ')[1];
+    const token = req.headers['authorization'].split(' ')[1];
 
     jsonwebtoken.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if (err) {
